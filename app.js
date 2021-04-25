77666fed5e6e11acf8ebaf9d4e2904d033c71b0c
Brymmobaggins <bakareibrahim98@gmail.com>
2021-04-25 16:05:21 +0100

    Updated Project

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var music = [
     "Jay Z ft Alicia keys - Empire state of Mind.",
     "Nasir jones - The world is ours",
+    "Wizkid ft Burna Boy - Ginger",
     "Craig David - 7 days.",
     "Wizkid ft Drake and Skepta - Ojuelegba remix.",
     "Micheal jackson - Billie jean.",
@@ -9,8 +10,20 @@ var music = [
     "Kanye west - No church in the wild",
     "Sade Adu - sweetest taboo.",
     "Kanye west - No church in the wild",
-    "larry gaga - we got served.",
-    "chris brown - Privacy"
+    "Naira Marley - Jo Soapy",
+    "Naira Marley - Marlians Anthem",
+    "Burna boy - Ye",
+    "Burna boy - odoqwu",
+    "Omahlay - My brother",
+    "Zlatan Ibile- Money no dey lagos",
+    "Davido - fem",
+    "Davido - jowo",
+    "Olamide ft badboy tems - Loading",
+    "Burna boy - Ye",
+    "Qdot - ah",
+    "Tems - like me",
+    "Rema - lady",
+    "Zinoleesky - Kilofeshe",
 
 ];
 
